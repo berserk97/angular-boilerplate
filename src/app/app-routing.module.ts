2d2e2d88d3d06c32092cca47e7505c2d4712e604
berserk97 <berserk200797@gmail.com>
2020-04-20 15:36:54 -0500

    Error handler implementation and modules clean up.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -32,28 +32,13 @@ const routes: Routes = [
 
   // response
   {
-    path: 'bad-request',
-    loadChildren: () => import('./pages/responses/bad-request/bad-request.module').then(m => m.BadRequestModule)
-  }, {
-    path: 'unauthorized',
-    loadChildren: () => import('./pages/responses/unauthorized/unauthorized.module').then(m => m.UnauthorizedModule)
-  }, {
-    path: 'forbidden',
-    loadChildren: () => import('./pages/responses/forbidden/forbidden.module').then(m => m.ForbiddenModule)
-  }, {
     path: 'not-found',
     loadChildren: () => import('./pages/responses/not-found/not-found.module').then(m => m.NotFoundModule)
-  }, {
-    path: 'method-not-allowed',
-    loadChildren: () => import('./pages/responses/method-not-allowed/method-not-allowed.module').then(m => m.MethodNotAllowedModule)
   }, {
     path: 'internal-server-error',
     loadChildren: () => import('./pages/responses/internal-server-error/internal-server-error.module')
       .then(m => m.InternalServerErrorModule)
-  }, {
-    path: 'service-unavailable',
-    loadChildren: () => import('./pages/responses/service-unavailable/service-unavailable.module').then(m => m.ServiceUnavailableModule)
-  },
+  }
 
   // features
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// classes
+import { GlobalErrorHandler } from './core/classes/global-error-handler';
+
+// interceptors
+import { ServerErrorInterceptor } from './core/interceptors/server-error.interceptor';
+
+// modules
 import { NavbarModule, FooterModule } from './ui';
 
 
@@ -16,7 +25,10 @@ import { NavbarModule, FooterModule } from './ui';
     NavbarModule,
     FooterModule
   ],
-  providers: [],
+  providers: [
+    { provide: ErrorHandler, useClass: GlobalErrorHandler },
+    { provide: HTTP_INTERCEPTORS, useClass: ServerErrorInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
